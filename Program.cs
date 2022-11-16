715af656d753e973496a554774b2251aae658fb2
Dinar <dinar-wp9@mail.ru>
2022-11-16 22:41:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Xml.Linq;
+
+namespace TryCatchPractices
+{
+    class MyException : Exception
+    {
+        public MyException(string message = "myException") : base(message)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Exception> exceptions = new List<Exception>()
+                {
+                    new MyException(),
+                    new ArgumentNullException(),
+                    new  DriveNotFoundException(),
+                    new FormatException(),
+                    new  IndexOutOfRangeException()
+                };
+                foreach (Exception ex in exceptions)
+                {
+                    try
+                    {
+                        throw ex;
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
